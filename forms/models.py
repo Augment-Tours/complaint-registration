07413b46dc2ce7d500c3e3ac7e3a2fa50c5f1dec
Amanuel Teferi <aman.teferi.80@gmail.com>
2021-06-25 06:29:13 +0300

    (fix) create form Fields and form field response model

--- forms/models.py
+++ forms/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 from django import forms
+from model_utils import Choices
 
 # Create your models here.
 class Form(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
-class FormTextBox(forms.CharField, models.Model):
-    # max_length
-    
+class FormField(models.Model):
+    FORM_TYPE = Choices('textbox', 'multiline_textbox')
+    type = models.CharField(choices=FORM_TYPE, max_length=50)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    hint = models.CharField(max_length=50, null=True, blank=True)
+    label = models.CharField(max_length=50, null=True, blank=True)
+
+    form = models.ForeignKey(Form,
+                             related_name='form',
+                             null=True,
+                             on_delete=models.SET_NULL)
+    data = models.JSONField()
+
+class FormFieldResponse(models.Model):
+    form_field = models.ForeignKey(FormField,
+                                   related_name='form_field',
+                                   null=True,
+                                   on_delete=models.SET_NULL)
+
+    data = models.JSONField()
--- forms/views.py
+++ forms/views.py
@@ -1,10 +1,9 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 
+from .models import Form, FormField, For
+
 class TestView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'test': 'abcd'
-        }
 
         return Response(data, status=status.HTTP_200_OK)
