9eb3bf748da270eb0e69c4526f54bbd61de7e634
Amanuel Teferi <aman.teferi.80@gmail.com>
2021-06-26 11:12:56 +0300

    (fix) change application name from shilengae_test to shilengae

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shilengae_test.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shilengae.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- shilengae/wsgi.py
+++ shilengae/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shilengae_test project.
+WSGI config for shilengae project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shilengae_test.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shilengae.settings')
 
 application = get_wsgi_application()
