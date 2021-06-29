130528e76673d316024a8d32249f008b34575530
Amanuel Teferi <aman.teferi.80@gmail.com>
2021-06-29 16:26:04 +0300

    (fix) edit country api view and endpoint added

--- locations/urls.py
+++ locations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path, re_path
 from rest_framework import routers
-from .views import CreateCountryApiView
+from .views import CreateCountryApiView, EditCountryApiView
 
 urlpatterns = [
     re_path(r'^country/create/$', CreateCountryApiView().as_view(), name='create_country'),
+    re_path(r'^country/edit/$', EditCountryApiView().as_view(), name='edit_country')
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 
@@ -6,3 +7,16 @@ from .serializers import CountrySerializer
 
 class CreateCountryApiView(generics.CreateAPIView):
     serializer_class = CountrySerializer
+
+class EditCountryApiView(generics.GenericAPIView):
+    serializer_class = CountrySerializer
+
+    def post(self, request, *args, **kwargs):
+        country_id = request.data.get("country_id")
+        country = get_object_or_404(Country, pk=country_id)
+
+        serializer = self.get_serializer(country, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_200_OK)
