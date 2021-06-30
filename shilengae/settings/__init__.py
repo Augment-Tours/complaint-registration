81dcc046bf31be75dabd0fd4813b786ba7771ca2
Amanuel Teferi <aman.teferi.80@gmail.com>
2021-06-30 09:29:50 +0300

    (fix) database configuration for heroku backend postgres db

---
+++ shilengae/settings/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+ENV_KEY = 'HEROKU_PROJECT'
+PROD_APPLICATION_ID = 'shilengae-backend-prod'
+
+if os.getenv(ENV_KEY, None) == PROD_APPLICATION_ID:
+    from .production import *
+else:
+    # TODO: change to testing
+    from .testing import *
---
+++ shilengae/settings/production.py
@@ -0,0 +1,11 @@
+from .testing import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'ec2-54-216-185-51.eu-west-1.compute.amazonaws.com',
+        'USER': 'qfabzxxrlgwpyo',
+        'PASSWORD': '2bfce99275f58fcdaab244db27347d19f0ebc3c91455721e3797cf6b7a77da04',
+        'NAME': 'd3qvf8bequhmqq',
+    }
+}
