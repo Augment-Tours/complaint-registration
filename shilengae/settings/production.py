383d5aa05119d97fc5df49b9047be2224bdc6fa6
Amanuel Teferi <aman.teferi.80@gmail.com>
2021-08-29 21:05:28 +0300

    (fix) minor

--- shilengae/settings/production.py
+++ shilengae/settings/production.py
@@ -2,7 +2,7 @@ from .testing import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': 'ec2-54-216-185-51.eu-west-1.compute.amazonaws.com',
         'USER': 'qfabzxxrlgwpyo',
         'PASSWORD': '2bfce99275f58fcdaab244db27347d19f0ebc3c91455721e3797cf6b7a77da04',
